ca5dd0fc8cd443a2392ab097ed0aaa73acc29cb3
zelouchan <bad1dje@gmail.com>
2024-12-06 15:26:24 +0000

    Adjusted some CSS, started JS

---
+++ script.js
@@ -0,0 +1,53 @@
+
+const date = new Date();
+
+const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const currentDayOfWeek = daysOfWeek[date.getDay()];
+
+// console.log(" It is" + currentDayOfWeek + " " + week )
+console.log(date);
+
+if date = (how to put in dates) {
+Week 0}
+
+else if (week days) {
+    Week 1
+}
+
+else if (dayes) {
+    Week 2
+} 
+
+else if (dayes) {
+    Week 3
+}
+
+else if (dayes) {
+    Week 4
+}
+
+else if (dayes) {
+    Week 5
+}
+
+else if (dayes) {
+    Week 6
+}
+
+else if (dayes) {
+    Week 7
+}
+
+else if (dayes) {
+    Week 8
+}
+
+else if (dayes) {
+    Week 9
+}
+
+else if (dayes) {
+    Week 10
+}
+
+else {No Term}
